4701f07aea3eb162906a6650c5d5bf45cebfc454
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2019-11-24 10:49:50 -0300

    inicio del repositorio

---
+++ server/server.js
@@ -0,0 +1,25 @@
+require('./config/config');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(require('./routes/routes'));
+
+mongoose.connect('mongodb://localhost:27017/MERN',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }, (err, res) => {
+        if (err) throw err;
+        console.log("Conexión realizada correctamente");
+    });
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server ON puerto ${process.env.PORT}`);
+});
